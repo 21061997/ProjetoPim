22698a109eb1db8ee339343de21c3e00eb005cfc
yuritj.yuri <yuritj.yuri@gmail.com>
2018-11-10 16:29:25 -0200

    Primeira alteração

--- HelpDeskLogin/Controllers/comentariosController.cs
+++ HelpDeskLogin/Controllers/comentariosController.cs
@@ -19,6 +19,8 @@ namespace HelpDeskLogin.Controllers
             _context = context;
         }
 
+       //ksbfjkbs
+
         // GET: comentarios
         public async Task<IActionResult> Index()
         {
